4fbf9ff8035615c640e06d26b2980fd74b51e615
yennhi2602 <nhi96942@gmail.com>
2025-07-17 09:39:22 +0700

    Add jest.config.js cấu hình test FE

---
+++ frontend/jest.config.js
@@ -0,0 +1,12 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+  },
+};
